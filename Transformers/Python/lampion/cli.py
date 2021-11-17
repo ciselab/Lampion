71f9c42b3f75e7cad5301b8568c5723d986a0696
lapplislazuli <Leonhard.Applis@Protonmail.com>
2021-11-17 10:13:35 +0100

    Implemented reading multiple files

--- Transformers/Python/lampion/cli.py
+++ Transformers/Python/lampion/cli.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 import libcst as cst
@@ -11,23 +12,62 @@ def dry_run(path):
     log.info(f'Welcome to the Lampion-Python-Transformer')
 
     log.info(f"Reading File(s) from {path}")
-    # Read a sample file with hellow world
-    f = file_to_string(path)
-    sample_cst = cst.parse_module(f)
+
+    csts = read_input_dir(path)
 
     engine = Engine({}, "PLACEHOLDER")
 
-    some = engine.run([sample_cst])[0]
+    some = engine.run(csts)[0]
     log.debug("========================")
-    log.debug(sample_cst.code)
+    log.debug(csts[0].code)
     log.debug("========================")
     log.debug("         Goes To        ")
     log.debug("========================")
     log.debug(some.code)
 
 
-def file_to_string(path):
-    # TODO: Check Folder vs. File
+def read_input_dir(path: str) -> ["Node"]:
+    """
+    This method parses a given path to one or more Libcst Modules.
+    It handles that you can pass the path to one file or to a folder .
+    In case of a folder, all .py files are read in.
+
+    :param path: The (relative or absolute) path to either a .py-file or a folder containing .py-files.
+    :return: the parsed libcst modules of the found python files
+    :raises: ValueError if the path was not found / did not exist
+    :raises: LibCst Error if the files were faulty and not parsable.
+    """
+    # ErrorCase: Path does not exist.
+    if not os.path.exists:
+        raise ValueError("Path was not found!")
+
+    # Case 1: Path is a directory
+    if os.path.isdir(path):
+        log.debug("Received Path is a Directory - Looking for multiple files")
+        results = []
+        for dirpath, dnames, fnames in os.walk(path):
+            #TODO: Create a lookup of files:cst ?
+            for f in fnames:
+                if f.endswith(".py"):
+                    ff = _file_to_string(os.path.join(dirpath, f))
+                    found_cst = cst.parse_module(ff)
+                    results.append(found_cst)
+        return results
+    # Case 2: Path is a file
+    elif os.path.isfile(path):
+        log.debug("Received Path points to a File")
+        # ErrorCase: File is not .py
+        if not path.endswith(".py"):
+            raise ValueError("File Path did not end in .py!")
+        f = _file_to_string(path)
+        found_cst = cst.parse_module(f)
+        return [found_cst]
+    # ErrorCase: Weird Path
+    else:
+        raise ValueError("Your path seemed to be neither a directory nor a file!")
+
+
+def _file_to_string(path: str):
     with open(path, 'r') as file:
         return file.read()
 
