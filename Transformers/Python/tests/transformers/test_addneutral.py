1095ba8eb7e5215f47199e4db778e3d1546fe9b1
Leonhard Applis <Leonhard.Applis@Protonmail.com>
2022-02-10 10:59:20 +0100

    Added Seeds to Add Neutral Tests

--- Transformers/Python/tests/transformers/test_addneutral.py
+++ Transformers/Python/tests/transformers/test_addneutral.py
@@ -11,6 +11,7 @@ from lampion.transformers.addneutral import AddNeutralElementTransformer, _reduc
 def test_addneutral_for_float_should_add_plus_0():
     example_cst = libcst.parse_module("def some(): \n\ta = 0.5 \n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -23,6 +24,7 @@ def test_addneutral_for_float_should_add_plus_0():
 def test_addneutral_apply_twice_for_float_should_add_plus_0():
     example_cst = libcst.parse_module("def some(): \n\ta = 0.5 \n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -38,6 +40,7 @@ def test_addneutral_apply_twice_for_float_should_add_plus_0():
 def test_addneutral_apply_to_method_with_two_floats_should_be_applied_once_only():
     example_cst = libcst.parse_module("def some(): \n\ta = 0.5\n\tb=0.3 \n\treturn a+b")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -51,6 +54,7 @@ def test_addneutral_apply_to_method_with_two_floats_should_be_applied_once_only(
 def test_addneutral_apply_to_method_with_two_identical_floats_should_be_applied_is_applied_at_first_instance():
     example_cst = libcst.parse_module("def some(): \n\ta = 0.5\n\tb=0.5 \n\treturn a+b")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -64,6 +68,7 @@ def test_addneutral_apply_to_method_with_two_identical_floats_should_be_applied_
 def test_addneutral_apply_twice_for_float_should_work():
     example_cst = libcst.parse_module("def some(): \n\ta = 0.5 \n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -71,7 +76,6 @@ def test_addneutral_apply_twice_for_float_should_work():
 
     transformer.reset()
     altered_cst = transformer.apply(altered_cst)
-    altered_code = altered_cst.code
 
     assert transformer.worked()
 
@@ -79,6 +83,7 @@ def test_addneutral_apply_twice_for_float_should_work():
 def test_addneutral_for_float_in_return_should_add_plus_0():
     example_cst = libcst.parse_module("def some():\n\treturn 0.5")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -91,6 +96,7 @@ def test_addneutral_for_float_in_return_should_add_plus_0():
 def test_addneutral_for_float_in_default_parameters_should_add_plus_0():
     example_cst = libcst.parse_module("def some(a: float = 0.5):\n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -104,11 +110,11 @@ def test_addneutral_for_float_in_default_parameters_should_add_plus_0():
 def test_addneutral_for_float_in_default_parameters_should_work():
     example_cst = libcst.parse_module("def some(a: float = 0.5):\n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
     altered_cst = transformer.apply(example_cst)
-    altered_code = altered_cst.code
 
     assert transformer.worked()
 
@@ -116,11 +122,11 @@ def test_addneutral_for_float_in_default_parameters_should_work():
 def test_addneutral_for_float_should_have_worked():
     example_cst = libcst.parse_module("def some(): \n\ta = 0.5 \n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
     altered_cst = transformer.apply(example_cst)
-    altered_code = altered_cst.code
 
     assert transformer.worked()
 
@@ -130,6 +136,7 @@ def test_addneutral_for_float_should_have_worked():
 def test_addneutral_for_string_should_add_plus_empty_string():
     example_cst = libcst.parse_module("def some(): \n\ta = \"text\" \n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -142,6 +149,7 @@ def test_addneutral_for_string_should_add_plus_empty_string():
 def test_addneutral_apply_twice_for_string_should_add_plus_emptystrings():
     example_cst = libcst.parse_module("def some(): \n\ta = \"text\" \n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -157,6 +165,7 @@ def test_addneutral_apply_twice_for_string_should_add_plus_emptystrings():
 def test_addneutral_apply_to_method_with_two_different_strings_should_be_applied_once_only():
     example_cst = libcst.parse_module("def some(): \n\ta =\"hello\"\n\tb=\"world\" \n\treturn a+b")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -170,6 +179,7 @@ def test_addneutral_apply_to_method_with_two_different_strings_should_be_applied
 def test_addneutral_apply_to_method_with_two_identical_string_should_be_applied_is_applied_at_first_instance():
     example_cst = libcst.parse_module("def some(): \n\ta = \"hello\"\n\tb=\"hello\" \n\treturn a+b")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -183,6 +193,7 @@ def test_addneutral_apply_to_method_with_two_identical_string_should_be_applied_
 def test_addneutral_apply_twice_for_string_should_work():
     example_cst = libcst.parse_module("def some(): \n\ta = \"hello\" \n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -190,7 +201,6 @@ def test_addneutral_apply_twice_for_string_should_work():
 
     transformer.reset()
     altered_cst = transformer.apply(altered_cst)
-    altered_code = altered_cst.code
 
     assert transformer.worked()
 
@@ -198,6 +208,7 @@ def test_addneutral_apply_twice_for_string_should_work():
 def test_addneutral_for_string_in_return_should_add_plus_emptystring():
     example_cst = libcst.parse_module("def some():\n\treturn \"world\"")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -210,6 +221,7 @@ def test_addneutral_for_string_in_return_should_add_plus_emptystring():
 def test_addneutral_for_string_in_default_parameters_should_add_plus_0():
     example_cst = libcst.parse_module("def some(a: str = \"hello\"):\n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -223,11 +235,11 @@ def test_addneutral_for_string_in_default_parameters_should_add_plus_0():
 def test_addneutral_for_string_in_default_parameters_should_work():
     example_cst = libcst.parse_module("def some(a: str = \"hello\"):\n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
     altered_cst = transformer.apply(example_cst)
-    altered_code = altered_cst.code
 
     assert transformer.worked()
 
@@ -235,6 +247,7 @@ def test_addneutral_for_string_in_default_parameters_should_work():
 def test_addneutral_for_string_should_have_worked():
     example_cst = libcst.parse_module("def some(): \n\ta = \"hello\" \n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -242,6 +255,7 @@ def test_addneutral_for_string_should_have_worked():
     altered_code = altered_cst.code
 
     assert transformer.worked()
+
 
 def test_addneutral_add_a_lot_of_strings_should_fail_gracefully():
     # There is an issue with adding too many brackets to strings
@@ -250,19 +264,22 @@ def test_addneutral_add_a_lot_of_strings_should_fail_gracefully():
     # I opened an issue with LibCST https://github.com/Instagram/LibCST/issues/640
     example_cst = libcst.parse_module("def some(): \n\ta = \"hello\" \n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     altered_cst = example_cst
-    for i in range(1,90):
+    for i in range(1, 90):
         transformer.reset()
         altered_cst = transformer.apply(altered_cst)
 
     assert True
 
+
 # Integers
 def test_addneutral_for_int_should_add_plus_0_variant_a():
     example_cst_a = libcst.parse_module("def some(): \n\ta = 5 \n\treturn a")
 
-    transformer_a = AddNeutralElementTransformer(max_tries=160)
+    random.seed(1996)
+    transformer_a = AddNeutralElementTransformer(max_tries=120)
     transformer_a.reset()
 
     altered_cst_a = transformer_a.apply(example_cst_a)
@@ -271,14 +288,14 @@ def test_addneutral_for_int_should_add_plus_0_variant_a():
 
     assert transformer_a.worked()
     assert "(5+0)" in altered_code_a
-    #TODO: Investigate why this is flaky!
-    #The first integer test fails, the others pass
+
 
 def test_addneutral_for_int_should_add_plus_0_variant_b():
     # Something was odd with adding 0 to 5, so I do a second test
     example_cst_b = libcst.parse_module("def some(): \n\ta = 33 \n\treturn a")
 
-    transformer_b = AddNeutralElementTransformer()
+    random.seed(1996)
+    transformer_b = AddNeutralElementTransformer(max_tries=120)
 
     altered_cst_b = transformer_b.apply(example_cst_b)
 
@@ -286,11 +303,13 @@ def test_addneutral_for_int_should_add_plus_0_variant_b():
     assert transformer_b.worked()
     assert "(33+0)" in altered_code
 
+
 def test_addneutral_for_int_should_add_plus_0_variant_c():
     # Something was odd with adding 0 to 5, so I do a third test
     example_cst_c = libcst.parse_module("def some(): \n\ta = 1 \n\treturn a")
 
-    transformer_c = AddNeutralElementTransformer()
+    random.seed(1996)
+    transformer_c = AddNeutralElementTransformer(max_tries=120)
     transformer_c.reset()
 
     altered_cst_c = transformer_c.apply(example_cst_c)
@@ -299,9 +318,11 @@ def test_addneutral_for_int_should_add_plus_0_variant_c():
     assert transformer_c.worked()
     assert "(1+0)" in altered_code
 
+
 def test_addneutral_apply_twice_for_int_should_add_plus_0():
     example_cst = libcst.parse_module("def some(): \n\ta = 5 \n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -319,6 +340,7 @@ def test_addneutral_apply_twice_for_int_should_add_plus_0():
 def test_addneutral_apply_to_method_with_two_different_ints_should_be_applied_once_only():
     example_cst = libcst.parse_module("def some(): \n\ta = 5\n\tb=3 \n\treturn a+b")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -332,19 +354,13 @@ def test_addneutral_apply_to_method_with_two_different_ints_should_be_applied_on
 def test_addneutral_apply_to_method_with_two_identical_ints_should_be_applied_is_applied_at_first_instance():
     example_cst = libcst.parse_module("def some(): \n\ta = 4\n\tb = 4 \n\treturn a+b")
 
-    # This Fails for some seeds???
-    random.seed(50)
+    random.seed(1996)
 
-    print("Before:\n")
-    print(str(example_cst.code))
-
-    transformer = AddNeutralElementTransformer()
+    transformer = AddNeutralElementTransformer(max_tries=150)
     transformer.reset()
 
     altered_cst = transformer.apply(example_cst)
     altered_code = str(altered_cst.code)
-    print("After:\n")
-    print(altered_code)
 
     assert "0" in altered_code
     assert altered_code.count("0") == 1
@@ -353,12 +369,12 @@ def test_addneutral_apply_to_method_with_two_identical_ints_should_be_applied_is
 def test_addneutral_apply_to_method_with_two_identical_ints_should_work():
     example_cst = libcst.parse_module("def some(): \n\ta = 4\n\tb = 4 \n\treturn a+b")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
     altered_cst = transformer.apply(example_cst)
     altered_code = altered_cst.code
-    print(altered_code)
 
     assert transformer.worked()
 
@@ -366,6 +382,7 @@ def test_addneutral_apply_to_method_with_two_identical_ints_should_work():
 def test_addneutral_apply_twice_for_int_should_work():
     example_cst = libcst.parse_module("def some(): \n\ta = 5 \n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer(max_tries=150)
     transformer.reset()
 
@@ -373,14 +390,13 @@ def test_addneutral_apply_twice_for_int_should_work():
 
     transformer.reset()
     altered_cst = transformer.apply(altered_cst)
-    altered_code = altered_cst.code
-
     assert transformer.worked()
 
 
 def test_addneutral_for_int_in_return_should_add_plus_0():
     example_cst = libcst.parse_module("def some():\n\treturn 5")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer(max_tries=150)
     transformer.reset()
 
@@ -393,6 +409,7 @@ def test_addneutral_for_int_in_return_should_add_plus_0():
 def test_addneutral_for_int_in_default_parameters_should_add_plus_0():
     example_cst = libcst.parse_module("def some(a: int = 5):\n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer(max_tries=120)
     transformer.reset()
 
@@ -406,11 +423,11 @@ def test_addneutral_for_int_in_default_parameters_should_add_plus_0():
 def test_addneutral_for_int_in_default_parameters_should_work():
     example_cst = libcst.parse_module("def some(a: int = 5):\n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer(max_tries=120)
     transformer.reset()
 
     altered_cst = transformer.apply(example_cst)
-    altered_code = altered_cst.code
 
     assert transformer.worked()
 
@@ -418,23 +435,25 @@ def test_addneutral_for_int_in_default_parameters_should_work():
 def test_addneutral_for_int_should_have_worked():
     example_cst = libcst.parse_module("def some(): \n\ta = 5 \n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
     altered_cst = transformer.apply(example_cst)
-    altered_code = altered_cst.code
 
     assert transformer.worked()
+
 
 def test_addneutral_for_int_apply_many_times_should_have_worked():
     example_cst = libcst.parse_module("def some(): \n\ta = 5 \n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer(max_tries=10)
     altered_cst = example_cst
     counter = 0
     many_times = 20
 
-    for i in range(0,many_times):
+    for i in range(0, many_times):
         altered_cst = transformer.apply(altered_cst)
         counter = counter + 1 if transformer.worked() else counter
         transformer.reset()
@@ -442,39 +461,37 @@ def test_addneutral_for_int_apply_many_times_should_have_worked():
     print(counter)
 
     altered_code = altered_cst.code
-    print(altered_code)
-    assert altered_code.count("0")  >= many_times / 2
+    assert altered_code.count("0") >= many_times / 2
+
 
 def test_addneutral_for_string_apply_many_times_should_have_worked():
     example_cst = libcst.parse_module("def some(): \n\ta = \"hello\" \n\treturn a")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer(max_tries=10)
     altered_cst = example_cst
     counter = 0
     many_times = 20
 
-
-    for i in range(1,many_times):
+    for i in range(1, many_times):
         altered_cst = transformer.apply(altered_cst)
         counter = counter + 1 if transformer.worked() else counter
         transformer.reset()
 
-    print(counter)
-
     altered_code = altered_cst.code
-    print(altered_code)
     assert altered_code.count("\"\"") >= many_times / 2
+
 
 # Empty Methods // No Literals
 
 def test_addneutral_method_has_no_literals_transformer_did_not_work():
     example_cst = libcst.parse_module("def some(): return Math.Pi")
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
     altered_cst = transformer.apply(example_cst)
-    altered_code = altered_cst.code
 
     assert not transformer.worked()
 
@@ -483,6 +500,7 @@ def test_addneutral_method_has_no_literals_code_did_not_change():
     example_cst = libcst.parse_module("def some(): return Math.Pi")
     initial_code = str(example_cst.code)
 
+    random.seed(1996)
     transformer = AddNeutralElementTransformer()
     transformer.reset()
 
@@ -501,12 +519,14 @@ def test_reduce_brackets_for_strings():
 
     assert result == expected_output
 
+
 def test_reduce_brackets_for_strings_stacked_string():
     input = '(("X" + "" + "" + "") + "")'
     expected_output = "(\"X\" + \"\" + \"\" + \"\" + \"\")"
     result = _reduce_brackets(input)
 
     assert result == expected_output
+
 
 def test_reduce_brackets_for_strings_nothing_to_reduce_remains_unchanged():
     input = '("X" + "" + "" + "")'
@@ -515,12 +535,14 @@ def test_reduce_brackets_for_strings_nothing_to_reduce_remains_unchanged():
 
     assert result == input
 
+
 def test_reduce_brackets_for_ints():
     input = "((5 + 0) + 0)"
     expected_output = "(5 + 0 + 0)"
     result = _reduce_brackets(input)
 
     assert result == expected_output
+
 
 def test_reduce_brackets_for_ints_stacked_ints():
     input = '((5 + 0 + 0 + 0) + 0)'
@@ -529,6 +551,7 @@ def test_reduce_brackets_for_ints_stacked_ints():
 
     assert result == expected_output
 
+
 def test_reduce_brackets_for_ints_nothing_to_reduce_remains_unchanged():
     input = '(5 + 0 + 0 + 0)'
 
@@ -536,12 +559,14 @@ def test_reduce_brackets_for_ints_nothing_to_reduce_remains_unchanged():
 
     assert result == input
 
+
 def test_reduce_brackets_mixed_elements_remains_unchanged():
     input = '((("5" + 0.0) + "") + 0)'
 
     result = _reduce_brackets(input)
 
     assert result == input
+
 
 # Learning Regex
 # (You always have to re-learn when you need it)
@@ -551,19 +576,20 @@ def test_replacing_brackets():
     # and is kept for documentation reasons
     sample = "((\"X\" + \"\") + \"\")"
     pattern = r'\(\("(.*?)" \+ ""\) \+ ""\)'
-    result_pattern =  r'("\1" + "" + "")'
+    result_pattern = r'("\1" + "" + "")'
 
-    result = re.sub(pattern,result_pattern, sample)
+    result = re.sub(pattern, result_pattern, sample)
 
     assert result.strip() == "(\"X\" + \"\" + \"\")"
+
 
 def test_replacing_brackets_stacked():
     # This test was useful to find the right Regex,
     # and is kept for documentation reasons
     sample = '(("X" + "" + "" + "") + "")'
     pattern = r'\(\("(.*?)" \+ ""\) \+ ""\)'
-    result_pattern =  r'("\1" + "" + "")'
+    result_pattern = r'("\1" + "" + "")'
 
-    result = re.sub(pattern,result_pattern, sample)
+    result = re.sub(pattern, result_pattern, sample)
 
     assert result.strip() == "(\"X\" + \"\" + \"\" + \"\" + \"\")"
